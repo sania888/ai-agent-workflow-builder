4712427dbd85018a1fdfb1c0b79b7face80937d2
Sania Thapa <saniathapa1970@gmail.com>
2026-08-10 14:46:15 +0530

    feat: add initial database schema

---
+++ hasura/migrations/default/20260810150000_initial_schema/down.sql
@@ -0,0 +1,9 @@
+DROP VIEW IF EXISTS public.organization_usage_this_month;
+
+DROP TABLE IF EXISTS public.step_runs;
+DROP TABLE IF EXISTS public.workflow_runs;
+DROP TABLE IF EXISTS public.workflow_triggers;
+DROP TABLE IF EXISTS public.workflow_steps;
+DROP TABLE IF EXISTS public.workflows;
+DROP TABLE IF EXISTS public.org_members;
+DROP TABLE IF EXISTS public.organizations;
